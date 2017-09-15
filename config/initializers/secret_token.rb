a2013397833a54c62ac60c975a8a4fe0e55b9530
Henri CLOCUH <henriclocuh@yahoo.fr>
2017-09-15 14:33:14 +0200

    Premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Henribook::Application.config.secret_token = '9872fd56fe853f2457be009d4627cc26428a11145239142f7f8d4b1fdf94c82b3d4c140cfd52396e1a36fab91185c7b90886664938c0d37bf072c2d3816ec46d'
